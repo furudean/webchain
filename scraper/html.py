07ad919a48a3752e795ef03372fa3b492ac34ad6
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2025-08-22 22:28:28 -0700

    only handle two

--- scraper/html.py
+++ scraper/html.py
@@ -49,4 +49,4 @@ async def get_node_nominations(html: str, root: str) -> list[str] | None:
 
     nominations = soup.head.find_all(is_valid_nomination)
 
-    return [str(tag.get('href')) for tag in nominations if isinstance(tag, Tag)]
+    return [str(tag.get('href')) for tag in nominations[:2] if isinstance(tag, Tag)]
