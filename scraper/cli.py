75352004a07fa83f1cc77d397ddff556d870afee
mara <mara@nekopath.fun>
2025-08-25 21:46:58 -0700

    my broken stuff

--- scraper/cli.py
+++ scraper/cli.py
@@ -2,7 +2,8 @@ import asyncio
 import sys
 
 from scraper.print import recursively_print_nominations
-
+from scraper.read import read_chain
+from scraper.crawl import get_session
 
 async def _print_tree():
     site = sys.argv[1] if len(sys.argv) > 1 else None
@@ -16,3 +17,10 @@ async def _print_tree():
 
 def print_tree():
     asyncio.run(_print_tree())
+
+
+async def _read():
+    await read_chain("https://chain-staging.milkmedicine.net")
+
+def read():
+    asyncio.run(_read())
