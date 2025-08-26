53068ec48af2fe69bf1567bc2168f8c896e24417
mara <mara@nekopath.fun>
2025-08-25 22:13:37 -0700

    whatington

--- scraper/cli.py
+++ scraper/cli.py
@@ -15,15 +15,12 @@ async def _print_tree():
 
     await recursively_print_nominations(site)
 
-
 def print_tree():
     asyncio.run(_print_tree())
-
 
 async def _read():
     async with get_session() as session:
         await read_chain('https://chain-staging.milkmedicine.net', session=session)
-
 
 def read():
     asyncio.run(_read())
