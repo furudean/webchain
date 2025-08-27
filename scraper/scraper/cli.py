edd18ac5494f808592d56cdc5eb43b8f564c5099
mara <mara@nekopath.fun>
2025-08-27 14:33:42 -0700

    added hash test functions

--- scraper/scraper/cli.py
+++ scraper/scraper/cli.py
@@ -5,7 +5,8 @@ import logging
 import os
 
 from scraper.crawl import crawl
-from scraper.read import recursively_read_nodes
+from scraper.read import read_chain_into_table
+from scraper.hash import HashTable
 
 def setup_logging(level='INFO'):
     logging.basicConfig(level=os.environ.get('LOGLEVEL', level).upper())
@@ -46,15 +47,27 @@ def get_json():
     asyncio.run(_get_json())
 
 
-async def _read_tree():
-    site = sys.argv[1] if len(sys.argv) > 1 else None
+async def _read_tree(insite=None) -> HashTable:
+    if not insite:
+        site = sys.argv[1] if len(sys.argv) > 1 else None
 
-    if not site:
-        print('usage: read-tree <root>')
-        sys.exit(1)
+        if not site:
+            print('usage: read-tree <root>')
+            sys.exit(1)
 
-    await recursively_read_nodes(site)
+        return await read_chain_into_table(site)
+    else:
+        return await read_chain_into_table(insite)
 
+def read_tree(root) -> HashTable:
+    return asyncio.run(_read_tree(root))
 
-def read_tree():
-    asyncio.run(_read_tree())
+def hash_test():
+    root = "https://chain-staging.milkmedicine.net"
+    T = read_tree(root)
+
+    found_node = T.findValue("https://chain-staging.milkmedicine.net/b/1")
+    print(found_node)
+    found_child = T.findValue(found_node.children[1])
+    print(found_child)
+    return
