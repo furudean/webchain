9d013d932a98186c5e01cbee8ff1e6eb6a7d434f
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2025-08-31 17:31:34 -0500

    zoom in and out!

--- web/src/routes/Graph.svelte
+++ web/src/routes/Graph.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	import { onMount } from "svelte"
-	import type { Sigma } from "sigma"
+	import type { Sigma, Camera } from "sigma"
 	import { calculateTreeLayout, buildGraph } from "$lib/graph"
 	import type { Node } from "$lib/node"
 
 	let graph_element: HTMLElement
 
 	let { nodes }: { nodes: Node[] } = $props()
+
+	function update_camera(camera: Camera) {
+		const size = `${100 / camera.ratio}px`
+		graph_element.style.backgroundSize = `${size} ${size}`
+
+		const pos_y = `${50 - (camera.x * 100) / camera.ratio}%`
+		const pos_x = `${50 + (camera.y * 100) / camera.ratio}%`
+		graph_element.style.backgroundPosition = `${pos_y} ${pos_x}`
+	}
 
 	onMount(() => {
 		let sigma_instance: Sigma | undefined
@@ -21,23 +30,25 @@
 				Object.values(nodes).map((node, i) => [i.toString(), node])
 			)
 
-			// Calculate layout and build graph
 			const positions = calculateTreeLayout(hashmap)
 			const graph = buildGraph(hashmap, positions, Graph)
-			// Create Sigma instance
 			sigma_instance = new Sigma(graph, graph_element, {
 				nodeProgramClasses: {
 					image: NodeImageProgram,
 					square: NodeSquareProgram
 				},
-				labelRenderedSizeThreshold: 10, // Only show labels when significantly zoomed in
-				labelSize: 10
+				labelRenderedSizeThreshold: 14,
+				labelSize: 10,
+				maxCameraRatio: 3,
+				minCameraRatio: 0.75
 			})
 
-			// Set default zoom to be more zoomed out
-			sigma_instance.getCamera().setState({
+			const camera = sigma_instance.getCamera()
+			camera.setState({
 				ratio: 1.2
 			})
+			camera.addListener("updated", update_camera)
+			update_camera(camera)
 		}
 
 		init_graph().catch(console.error)
@@ -59,12 +70,11 @@
 		position: fixed;
 		top: 0;
 		left: 0;
-	}
-
-	:global(body) {
-		background-size: 100px 100px;
-		/* background-image:
+		background-image:
 			linear-gradient(to right, hsl(0, 0%, 95%) 1px, transparent 1px),
-			linear-gradient(to bottom, hsl(0, 0%, 95%) 1px, transparent 1px); */
+			linear-gradient(to bottom, hsl(0, 0%, 95%) 1px, transparent 1px);
+		background-size: 83.3333px 83.3333px;
+		background-position: 8.33333% 91.6667%;
+		background-origin: 0 0;
 	}
 </style>
