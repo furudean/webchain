435c803727f44c882f23186e2aed9de683247975
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2025-09-08 18:54:45 -0700

    run scraper after deploy

--- scraper/deploy.sh
+++ scraper/deploy.sh
@@ -7,4 +7,7 @@ uv build
 
 scp dist/webchain_scraper-0.0.0-py3-none-any.whl webchain.milkmedicine.net:/root
 
-ssh webchain.milkmedicine.net "uv tool install webchain_scraper-0.0.0-py3-none-any.whl --force"
+ssh webchain.milkmedicine.net "
+	uv tool install webchain_scraper-0.0.0-py3-none-any.whl --force && \
+	/root/crawl.sh
+"
