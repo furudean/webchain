8365b29bc5a165754251483ae5cd83a0079596b6
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2025-10-05 19:26:10 -0700

    be more tolerant of errors

--- scraper/scraper/http.py
+++ scraper/scraper/http.py
@@ -1,7 +1,7 @@
 import logging
 
 import aiohttp
-from tenacity import retry, stop_after_attempt, wait_fixed
+import tenacity
 
 logger = logging.getLogger()
 
@@ -10,6 +10,8 @@ def get_session() -> aiohttp.ClientSession:
     return aiohttp.ClientSession(
         headers={'User-Agent': 'webchain-scraper/DRAFT', 'Accept-Language': 'en-US, *;q=0.5'},
         raise_for_status=True,
+        cookie_jar=aiohttp.DummyCookieJar(),
+        trust_env=True,
     )
 
 
@@ -17,9 +19,9 @@ class InvalidContentType(Exception):
     pass
 
 
-@retry(
-    wait=wait_fixed(3),
-    stop=stop_after_attempt(3),
+@tenacity.retry(
+    wait=tenacity.wait_exponential(),
+    stop=tenacity.stop_after_attempt(3),
     retry_error_callback=lambda state: None,  # return None if retries fail
 )
 async def load_page_html(
@@ -49,8 +51,8 @@ async def load_page_html(
         # server returned no response at all. candidate for retry
         logger.info(f'server connection error for url {url}: ' + type(e).__name__)
         raise
-    except (aiohttp.InvalidURL, aiohttp.ClientConnectionError) as e:
-        # some kind of low-level connection error (DNS failure, refused connection, etc)
+    except aiohttp.InvalidURL as e:
+        # some kind of low-level connection error (refused connection, etc)
         # we assume these are permanent and do not retry
         logger.info(f'could not connect to {url}: ' + type(e).__name__)
         return None
@@ -67,3 +69,6 @@ async def load_page_html(
     except InvalidContentType:
         logger.info(f'non-html content-type for url {url}: {content_type}')
         return None
+    except aiohttp.ClientError as e:
+        logger.warning(f'{url}: ' + type(e).__name__)
+        raise
