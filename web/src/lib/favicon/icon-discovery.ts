26231a8db706af9f62009c5a309f5145b630f2c4
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2025-10-11 11:56:30 -0700

    fetch favicons from the entire html, not just the head

--- web/src/lib/favicon/icon-discovery.ts
+++ web/src/lib/favicon/icon-discovery.ts
@@ -4,24 +4,28 @@ import { is_valid_url, safe_parse_url } from "$lib/url"
 
 export async function get_icon_urls(
 	base: URL | string,
-	head: HTMLElement | null
+	html: HTMLElement | null
 ): Promise<URL[]> {
-	const selectors = ['link[rel="icon"]', 'link[rel="shortcut icon"]']
+	const selectors = ['link[rel~="icon"]']
 	const possible_icons: Set<URL> = new Set()
 
-	if (head) {
+	if (html) {
+		// console.log(html.toString())
 		for (const selector of selectors) {
-			const element = head.querySelector(selector)
-			if (element?.hasAttribute("href")) {
-				const href = element.getAttribute("href")!
-				if (is_valid_url(href)) {
-					possible_icons.add(new URL(href))
-				} else {
-					const parsed_url = safe_parse_url(href, base)
-					if (parsed_url) {
-						possible_icons.add(parsed_url)
+			const elements = html.querySelectorAll(selector)
+			for (const element of elements) {
+				// console.log(base, element?.toString())
+				if (element?.hasAttribute("href")) {
+					const href = element.getAttribute("href")!
+					if (is_valid_url(href)) {
+						possible_icons.add(new URL(href))
 					} else {
-						console.log(`skipping invalid favicon url: ${href}`)
+						const parsed_url = safe_parse_url(href, base)
+						if (parsed_url) {
+							possible_icons.add(parsed_url)
+						} else {
+							console.log(`skipping invalid favicon url: ${href}`)
+						}
 					}
 				}
 			}
@@ -40,5 +44,5 @@ export async function parse_page_for_icons(
 	const parsed = parse(html)
 	if (!parsed) return []
 
-	return get_icon_urls(url, parsed.querySelector("head"))
+	return get_icon_urls(url, parsed)
 }
