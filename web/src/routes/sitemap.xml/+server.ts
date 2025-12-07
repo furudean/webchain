f822e72342f10e6a37735b47760066ca41ec5004
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2025-12-07 15:16:18 -0800

    sitemap

---
+++ web/src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,57 @@
+import { html } from "common-tags"
+import type { RequestHandler } from "./$types"
+import { get_current_crawl } from "$lib/crawler"
+import { entries as doc_entries } from "../doc/[slug]/+page.server"
+
+const static_routes = Object.keys(
+	import.meta.glob("/src/routes/**/+page.svelte")
+)
+	.filter((route) => !route.includes("[slug]"))
+	.map((page) => page.replace("/src/routes", "").replace("+page.svelte", ""))
+
+function create_url_element(
+	url: URL,
+	lastmod: Date | undefined = undefined
+): string {
+	return html`
+		<url>
+			<loc>${url.href}</loc>
+			${lastmod
+				? "<lastmod>" + lastmod.toISOString().slice(0, 10) + "</lastmod>"
+				: ""}
+		</url>
+	`
+}
+
+export const GET: RequestHandler = async ({ url, fetch: fetch_fn }) => {
+	const webchain_urls = (await get_current_crawl(fetch_fn)) ?? []
+	const documents = await doc_entries()
+
+	const url_elements = [
+		...static_routes.map((route) =>
+			create_url_element(new URL(route, url.protocol + url.host))
+		),
+		...documents.map((doc) =>
+			create_url_element(new URL(`/doc/${doc.slug}`, url.protocol + url.host))
+		),
+		...webchain_urls.map((node) =>
+			create_url_element(
+				new URL(node.at),
+				node.last_updated ? new Date(node.last_updated) : undefined
+			)
+		)
+	]
+
+	const xml = html`
+		<?xml version="1.0" encoding="UTF-8" ?>
+		<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+			${url_elements.join("\n")}
+		</urlset>
+	`
+
+	return new Response(xml, {
+		headers: {
+			"Content-Type": "application/xml"
+		}
+	})
+}
