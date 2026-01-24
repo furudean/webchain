237f0c2231489e4e3819b7edcddc50345788c6ce
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2026-01-24 15:49:36 -0800

    remove another unused head check

--- crawler/spider/http.py
+++ crawler/spider/http.py
@@ -31,18 +31,6 @@ async def load_page_html(
             headers["Referer"] = referrer
 
         try:
-            # HEAD request to check if robots.txt exists
-            async with session.head(
-                url, timeout=aiohttp.ClientTimeout(total=5), headers=headers
-            ) as head_response:
-                content_type = head_response.headers.get("Content-Type", "")
-                if not (
-                    content_type.startswith("text/html")
-                    or content_type.startswith("application/xhtml+xml")
-                ):
-                    raise InvalidContentType(content_type)
-
-            logger.debug(f"get {url}")
             async with session.get(
                 url, timeout=aiohttp.ClientTimeout(total=10), headers=headers
             ) as response:
