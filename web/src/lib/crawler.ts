86c8a4ca57fbcc8e0f205df71f7932e9f7b9f26a
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2026-01-24 23:05:17 -0800

    http cache on crawler

--- web/src/lib/crawler.ts
+++ web/src/lib/crawler.ts
@@ -1,34 +1,46 @@
-import type { CrawledNode, CrawlResponse } from "./node"
+import type { CrawlResponse } from "./node"
 
-const REFRESH_INTERVAL = 5 * 60_000 // 5 minutes
+const REFRESH_INTERVAL = 60_000 // 1 minute
 
-let current_crawl: CrawledNode[] | undefined = undefined
+let current_crawl: CrawlResponse | undefined = undefined
+let last_etag: string | null = null
 let last_fetch = 0
-let ongoing_fetch: Promise<Partial<CrawlResponse> | undefined> | null = null
+let ongoing_fetch: Promise<CrawlResponse | undefined> | null = null
 
-export async function get_current_crawl(
+async function get_current_crawl(
 	fetch_fn: typeof fetch
-): Promise<typeof current_crawl> {
+): Promise<CrawlResponse> {
 	const now = Date.now()
 	if (current_crawl && now - last_fetch < REFRESH_INTERVAL) {
 		return current_crawl
 	}
+
 	try {
+		const headers = new Headers()
+		if (last_etag) headers.append('if-none-match', last_etag)
+
 		// merge incoming request if another is ongoing
 		const request =
 			ongoing_fetch ??
-			fetch_fn("/crawler/current.json").then((response) => {
-				if (!response.ok)
-					throw new Error(`failed to fetch crawler data: ${response.status}`)
-				return response.json() as Promise<Partial<CrawlResponse>>
+			fetch_fn("/crawler/current.json", {
+				headers,
+			}).then((response) => {
+				if (response.ok) {
+					last_etag = response.headers.get('etag')
+					return response.json() as Promise<CrawlResponse>
+				}
+
+				if (response.status === 503 && current_crawl)
+					return current_crawl
+
+				throw new Error(`failed to fetch crawler data: ${response.status}`)
 			})
 		ongoing_fetch = request
-		const crawl = await request
+		current_crawl = await request
 		ongoing_fetch = null
 
-		current_crawl = crawl?.nodes
 		last_fetch = now
-		return current_crawl
+		return current_crawl!
 	} catch (err) {
 		console.error("failed to refresh allowed favicon URLs", err)
 		throw err
@@ -38,7 +50,7 @@ export async function get_current_crawl(
 export async function is_webchain_node(url: string, fetch_fn: typeof fetch) {
 	const crawl = await get_current_crawl(fetch_fn)
 	if (!crawl) throw new Error("no crawl data available")
-	return crawl.find((node) => node.at === url) !== undefined
+	return crawl.nodes.find((node) => node.at === url) !== undefined
 }
 
 /**
@@ -47,7 +59,7 @@ export async function is_webchain_node(url: string, fetch_fn: typeof fetch) {
 export async function robots_ok(at: string, fetch_fn: typeof fetch) {
 	const crawl = await get_current_crawl(fetch_fn)
 	if (!crawl) return true
-	const current_node = crawl.find((node) => node.at === at)
+	const current_node = crawl.nodes.find((node) => node.at === at)
 
 	return current_node?.robots_ok !== false
 }
